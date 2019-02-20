5502ebaad2f1676fb4e44794671c34f40042402c
Dafank <fentik9814@gmail.com>
2019-02-20 23:46:08 +0200

    Add controler and make some fixes

---
+++ Library/ProfileData.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Library
+{
+    partial class ProfileData
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lastNamelabel = new System.Windows.Forms.Label();
+            this.firstNamelabel = new System.Windows.Forms.Label();
+            this.middleNamelabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.booksAmount = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(223, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(308, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Information";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ravie", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(72, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(261, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Your Last Name - ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Ravie", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(72, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(270, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Your First Name - ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Ravie", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(72, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(288, 26);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Your Middle Name - ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Ravie", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(72, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(190, 26);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Your email - ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Ravie", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(73, 255);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(370, 26);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Your Date Registration - ";
+            // 
+            // lastNamelabel
+            // 
+            this.lastNamelabel.AutoSize = true;
+            this.lastNamelabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 17.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastNamelabel.Location = new System.Drawing.Point(332, 82);
+            this.lastNamelabel.Name = "lastNamelabel";
+            this.lastNamelabel.Size = new System.Drawing.Size(80, 27);
+            this.lastNamelabel.TabIndex = 6;
+            this.lastNamelabel.Text = "label7";
+            // 
+            // firstNamelabel
+            // 
+            this.firstNamelabel.AutoSize = true;
+            this.firstNamelabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 17.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstNamelabel.Location = new System.Drawing.Point(336, 123);
+            this.firstNamelabel.Name = "firstNamelabel";
+            this.firstNamelabel.Size = new System.Drawing.Size(80, 27);
+            this.firstNamelabel.TabIndex = 7;
+            this.firstNamelabel.Text = "label8";
+            // 
+            // middleNamelabel
+            // 
+            this.middleNamelabel.AutoSize = true;
+            this.middleNamelabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 17.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.middleNamelabel.Location = new System.Drawing.Point(355, 167);
+            this.middleNamelabel.Name = "middleNamelabel";
+            this.middleNamelabel.Size = new System.Drawing.Size(80, 27);
+            this.middleNamelabel.TabIndex = 8;
+            this.middleNamelabel.Text = "label9";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 17.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailLabel.Location = new System.Drawing.Point(259, 211);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(94, 27);
+            this.emailLabel.TabIndex = 9;
+            this.emailLabel.Text = "label10";
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 17.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateLabel.Location = new System.Drawing.Point(435, 255);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(94, 27);
+            this.dateLabel.TabIndex = 10;
+            this.dateLabel.Text = "label11";
+            // 
+            // booksAmount
+            // 
+            this.booksAmount.AutoSize = true;
+            this.booksAmount.Font = new System.Drawing.Font("Arial Rounded MT Bold", 17.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.booksAmount.Location = new System.Drawing.Point(309, 300);
+            this.booksAmount.Name = "booksAmount";
+            this.booksAmount.Size = new System.Drawing.Size(94, 27);
+            this.booksAmount.TabIndex = 12;
+            this.booksAmount.Text = "label11";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Ravie", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(73, 300);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(230, 26);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Books Amount - ";
+            // 
+            // ProfileData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.booksAmount);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.middleNamelabel);
+            this.Controls.Add(this.firstNamelabel);
+            this.Controls.Add(this.lastNamelabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ProfileData";
+            this.Size = new System.Drawing.Size(755, 353);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lastNamelabel;
+        private System.Windows.Forms.Label firstNamelabel;
+        private System.Windows.Forms.Label middleNamelabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label booksAmount;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ Library/ProfileData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataBaseLibrary;
+
+namespace Library
+{
+    public partial class ProfileData : UserControl
+    {
+        readonly int UserId;
+        DataBaseModel db;
+        public ProfileData(int Id)
+        {
+            InitializeComponent();
+            db = new DataBaseModel();
+            UserId = Id;
+            SetData();
+        }
+
+        public void SetData()
+        {
+            var user = from temp in db.UserInfoes
+                       where temp.UserID == UserId
+                       select new
+                       {
+                           LName = temp.LName,
+                           FName = temp.FName,
+                           MName = temp.MName,
+                           Email = temp.AutentnData.Email,
+                           Books = temp.Libraries.Where(x => x.BookOwner == temp.UserID).Count()
+                       };
+            foreach (var item in user)
+            {
+                lastNamelabel.Text = item.LName;
+                firstNamelabel.Text = item.FName;
+                middleNamelabel.Text = item.MName;
+                emailLabel.Text = item.Email;
+                dateLabel.Text = DateTime.Now.ToShortTimeString();
+                try
+                {
+                    booksAmount.Text = item.Books.ToString();
+                }
+                catch(Exception)
+                {
+                    booksAmount.Text = "0";
+                }
+                //TODO: Add Date here!
+            }
+                       
+        }
+
+    }
+}
